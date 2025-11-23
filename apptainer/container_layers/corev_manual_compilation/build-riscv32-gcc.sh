413c7bf6e91720e308a6f9baff0b22b3c66a0c06
allpaul0 <paulallaireperso@gmail.com>
2025-11-23 17:20:50 +0100

    add corev_manual_compilation folder for user defined multilib ISA_ABI combinations, state: doesnt work

---
+++ apptainer/container_layers/corev_manual_compilation/build-riscv32-gcc.sh
@@ -0,0 +1,225 @@
+#!/bin/bash -xe
+# Script for building a RISC-V GNU Toolchain from checked out sources
+
+# Copyright (C) 2020 Embecosm Limited
+
+# Contributor: Simon Cook <simon.cook@embecosm.com>
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Variables used in this script
+INSTALLPREFIX=/opt/tools/corev
+BUILDPREFIX=${PWD}/build
+SRCPREFIX=${PWD}
+
+# Allow the triple and default architecture and ABI to be overridden
+if [ "x${TRIPLE}" == "x" ]; then
+  TRIPLE=riscv32-unknown-elf
+fi
+if [ "x${DEFAULTARCH}" == "x" ]; then
+  DEFAULTARCH=rv32imac
+fi
+if [ "x${DEFAULTABI}" == "x" ]; then
+  DEFAULTABI=ilp32
+fi
+
+# OpenHW set of multilibs
+EXTRA_MULTI=""
+OPENHWMULTI="rv32im_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32-- \
+    rv32imc_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32--        \
+    rv32imf_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32f--       \
+    rv32imfc_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32f--      \
+    rv32im_zfinx_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32--   \
+    rv32imc_zfinx_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip-ilp32--"
+
+# Print the GCC and G++ used in this build
+which gcc
+which g++
+
+# If a BUGURL and PKGVERS has been provided, set variables
+EXTRA_OPTS=""
+if [ "x${BUGURL}" != "x" ]; then
+  EXTRA_OPTS="${EXTRA_OPTS} --with-bugurl='${BUGURL}'"
+fi
+if [ "x${PKGVERS}" != "x" ]; then
+  EXTRA_OPTS="${EXTRA_OPTS} --with-pkgversion='${PKGVERS}'"
+# If this is a corev build, use the OpenHW specific multilibs.
+  if [[ "${PKGVERS}" =~ "corev" ]]; then
+    EXTRA_MULTI="${OPENHWMULTI}"
+  fi
+fi
+
+# Allow environment to control parallelism
+if [ "x${PARALLEL_JOBS}" == "x" ]; then
+  PARALLEL_JOBS=$(nproc)
+fi
+
+# Binutils-gdb - Do in one step if possible
+if [ -e "binutils-gdb" ]; then
+  source utils/download-libgmp.sh binutils-gdb
+  mkdir -p ${BUILDPREFIX}/binutils-gdb
+  cd ${BUILDPREFIX}/binutils-gdb
+  CFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  CXXFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  ../../binutils-gdb/configure        \
+      --target=${TRIPLE}              \
+      --prefix=${INSTALLPREFIX}       \
+      --with-expat                    \
+      --disable-werror                \
+      ${EXTRA_OPTS}                   \
+      ${EXTRA_BINUTILS_OPTS}
+  make -j${PARALLEL_JOBS}
+  make install
+else
+  source utils/download-libgmp.sh gdb
+  # Binutils
+  mkdir -p ${BUILDPREFIX}/binutils
+  cd ${BUILDPREFIX}/binutils
+  CFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  CXXFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  ../../binutils/configure            \
+      --target=${TRIPLE}              \
+      --prefix=${INSTALLPREFIX}       \
+      --disable-werror                \
+      --disable-gdb                   \
+      ${EXTRA_OPTS}                   \
+      ${EXTRA_BINUTILS_OPTS}
+  make -j${PARALLEL_JOBS}
+  make install
+  # GDB
+  mkdir -p ${BUILDPREFIX}/gdb
+  cd ${BUILDPREFIX}/gdb
+  CFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  CXXFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
+  ../../gdb/configure                 \
+      --target=${TRIPLE}              \
+      --prefix=${INSTALLPREFIX}       \
+      --with-expat                    \
+      --disable-werror                \
+      ${EXTRA_OPTS}                   \
+      ${EXTRA_BINUTILS_OPTS}
+  make -j${PARALLEL_JOBS} all-gdb
+  make install-gdb
+fi
+
+# GCC
+cd ${SRCPREFIX}/gcc
+./contrib/download_prerequisites
+mkdir -p ${BUILDPREFIX}/gcc-stage1
+cd ${BUILDPREFIX}/gcc-stage1
+../../gcc/configure                                     \
+    --target=${TRIPLE}                                  \
+    --prefix=${INSTALLPREFIX}                           \
+    --with-sysroot=${INSTALLPREFIX}/${TRIPLE}           \
+    --with-newlib                                       \
+    --without-headers                                   \
+    --disable-shared                                    \
+    --enable-languages=c                                \
+    --disable-werror                                    \
+    --disable-libatomic                                 \
+    --disable-libmudflap                                \
+    --disable-libssp                                    \
+    --disable-quadmath                                  \
+    --disable-libgomp                                   \
+    --disable-nls                                       \
+    --disable-bootstrap                                 \
+    --enable-multilib                                   \
+    --with-multilib-generator="rv32e-ilp32e--c rv32ea-ilp32e--m rv32em-ilp32e--c rv32eac-ilp32e-- rv32emac-ilp32e-- rv32i-ilp32--c rv32ia-ilp32--m rv32im-ilp32--c rv32if-ilp32f-rv32ifd-c rv32iaf-ilp32f-rv32imaf,rv32iafc-d rv32imf-ilp32f-rv32imfd-c rv32iac-ilp32-- rv32imac-ilp32-- rv32imafc-ilp32f-rv32imafdc- rv32ifd-ilp32d--c rv32imfd-ilp32d--c rv32iafd-ilp32d-rv32imafd,rv32iafdc- rv32imafdc-ilp32d-- rv64i-lp64--c rv64ia-lp64--m rv64im-lp64--c rv64if-lp64f-rv64ifd-c rv64iaf-lp64f-rv64imaf,rv64iafc-d rv64imf-lp64f-rv64imfd-c rv64iac-lp64-- rv64imac-lp64-- rv64imafc-lp64f-rv64imafdc- rv64ifd-lp64d--m,c rv64iafd-lp64d-rv64imafd,rv64iafdc- rv64imafdc-lp64d-- ${EXTRA_MULTI}" \
+    --with-arch=${DEFAULTARCH}                          \
+    --with-abi=${DEFAULTABI}                            \
+    ${EXTRA_OPTS}                                       \
+    ${EXTRA_GCC_OPTS}
+make -j${PARALLEL_JOBS}
+make install
+
+# Newlib
+PATH=${INSTALLPREFIX}/bin:${PATH}
+mkdir -p ${BUILDPREFIX}/newlib
+cd ${BUILDPREFIX}/newlib
+CFLAGS_FOR_TARGET="-O2 -mcmodel=medany"            \
+../../newlib/configure                             \
+    --target=${TRIPLE}                             \
+    --prefix=${INSTALLPREFIX}                      \
+    --with-arch=${DEFAULTARCH}                     \
+    --with-abi=${DEFAULTABI}                       \
+    --enable-multilib                              \
+    --enable-newlib-io-long-double                 \
+    --enable-newlib-io-long-long                   \
+    --enable-newlib-io-c99-formats                 \
+    --enable-newlib-register-fini                  \
+    ${EXTRA_OPTS}                                  \
+    ${EXTRA_NEWLIB_OPTS}
+make -j${PARALLEL_JOBS}
+make install
+
+# Nano-newlib
+# NOTE: This configuration is based on the config.log of a "riscv-gnu-toolchain"
+# nano newlib library build
+mkdir -p ${BUILDPREFIX}/newlib-nano
+cd ${BUILDPREFIX}/newlib-nano
+CFLAGS_FOR_TARGET="-Os -mcmodel=medany -ffunction-sections -fdata-sections" \
+../../newlib/configure                             \
+    --target=${TRIPLE}                             \
+    --prefix=${BUILDPREFIX}/newlib-nano-inst       \
+    --with-arch=${DEFAULTARCH}                     \
+    --with-abi=${DEFAULTABI}                       \
+    --enable-multilib                              \
+    --enable-newlib-reent-small                    \
+    --disable-newlib-fvwrite-in-streamio           \
+    --disable-newlib-fseek-optimization            \
+    --disable-newlib-wide-orient                   \
+    --enable-newlib-nano-malloc                    \
+    --disable-newlib-unbuf-stream-opt              \
+    --enable-lite-exit                             \
+    --enable-newlib-global-atexit                  \
+    --enable-newlib-nano-formatted-io              \
+    --disable-newlib-supplied-syscalls             \
+    --disable-nls                                  \
+    ${EXTRA_OPTS}                                  \
+    ${EXTRA_NEWLIB_OPTS}
+make -j${PARALLEL_JOBS}
+make install
+
+# Manualy copy the nano variant to the expected location
+# Directory information obtained from "riscv-gnu-toolchain"
+for multilib in $(${INSTALLPREFIX}/bin/${TRIPLE}-gcc --print-multi-lib); do
+  multilibdir=$(echo ${multilib} | sed 's/;.*//')
+  for file in libc.a libm.a libg.a libgloss.a; do
+    cp ${BUILDPREFIX}/newlib-nano-inst/${TRIPLE}/lib/${multilibdir}/${file} \
+        ${INSTALLPREFIX}/${TRIPLE}/lib/${multilibdir}/${file%.*}_nano.${file##*.}
+  done
+  cp ${BUILDPREFIX}/newlib-nano-inst/${TRIPLE}/lib/${multilibdir}/crt0.o \
+      ${INSTALLPREFIX}/${TRIPLE}/lib/${multilibdir}/crt0.o
+done
+mkdir -p ${INSTALLPREFIX}/${TRIPLE}/include/newlib-nano
+cp ${BUILDPREFIX}/newlib-nano-inst/${TRIPLE}/include/newlib.h \
+    ${INSTALLPREFIX}/${TRIPLE}/include/newlib-nano/newlib.h
+
+# GCC stage 2
+cd ${SRCPREFIX}/gcc
+./contrib/download_prerequisites
+mkdir -p ${BUILDPREFIX}/gcc-stage2
+cd ${BUILDPREFIX}/gcc-stage2
+../../gcc/configure                                     \
+    --target=${TRIPLE}                                  \
+    --prefix=${INSTALLPREFIX}                           \
+    --with-sysroot=${INSTALLPREFIX}/${TRIPLE}           \
+    --with-native-system-header-dir=/include            \
+    --with-newlib                                       \
+    --disable-shared                                    \
+    --enable-languages=c,c++                            \
+    --enable-tls                                        \
+    --disable-werror                                    \
+    --disable-libmudflap                                \
+    --disable-libssp                                    \
+    --disable-quadmath                                  \
+    --disable-libgomp                                   \
+    --disable-nls                                       \
+    --enable-multilib                                   \
+    --with-multilib-generator="rv32e-ilp32e--c rv32ea-ilp32e--m rv32em-ilp32e--c rv32eac-ilp32e-- rv32emac-ilp32e-- rv32i-ilp32--c rv32ia-ilp32--m rv32im-ilp32--c rv32if-ilp32f-rv32ifd-c rv32iaf-ilp32f-rv32imaf,rv32iafc-d rv32imf-ilp32f-rv32imfd-c rv32iac-ilp32-- rv32imac-ilp32-- rv32imafc-ilp32f-rv32imafdc- rv32ifd-ilp32d--c rv32imfd-ilp32d--c rv32iafd-ilp32d-rv32imafd,rv32iafdc- rv32imafdc-ilp32d-- rv64i-lp64--c rv64ia-lp64--m rv64im-lp64--c rv64if-lp64f-rv64ifd-c rv64iaf-lp64f-rv64imaf,rv64iafc-d rv64imf-lp64f-rv64imfd-c rv64iac-lp64-- rv64imac-lp64-- rv64imafc-lp64f-rv64imafdc- rv64ifd-lp64d--m,c rv64iafd-lp64d-rv64imafd,rv64iafdc- rv64imafdc-lp64d-- ${EXTRA_MULTI}" \
+    --with-arch=${DEFAULTARCH}                          \
+    --with-abi=${DEFAULTABI}                            \
+    ${EXTRA_OPTS}                                       \
+    ${EXTRA_GCC_OPTS}
+make -j${PARALLEL_JOBS}
+make install
